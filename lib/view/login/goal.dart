3cbdd681b1406174b218c3578b591357b2bd1052
joviitt <jovitjohn2004@gmail.com>
2025-02-02 23:10:36 +0530

    commit

--- lib/view/login/goal.dart
+++ lib/view/login/goal.dart
@@ -28,14 +28,14 @@ class _WhatYourGoalViewState extends State<WhatYourGoalView> {
           "I have a low amount of body fat\nand need / want to build more\nmuscle"
     },
     {
-      "image": "assets/img/goal_2.png",
+      "image": "assets/images/lean.png",
       "title": "Lean & Tone",
       "subtitle":
           "I’m “skinny fat”. look thin but have\nno shape. I want to add learn\nmuscle in the right way"
     },
     {
-      "image": "assets/img/goal_3.png",
-      "title": "Lose a Fat",
+      "image": "assets/images/losefat.png",
+      "title": "Lose Fat",
       "subtitle":
           "I have over 20 lbs to lose. I want to\ndrop all this fat and gain muscle\nmass"
     },
