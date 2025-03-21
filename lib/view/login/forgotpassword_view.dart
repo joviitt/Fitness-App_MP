95720d8d369769788cc2bf87fb86bdb7a907b0b2
joviitt <jovitjohn2004@gmail.com>
2025-03-21 19:42:01 +0530

    commit

--- lib/view/login/forgotpassword_view.dart
+++ lib/view/login/forgotpassword_view.dart
@@ -11,6 +11,9 @@ class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.blue,
+      elevation: 0,),
+      
       body: Text('Enter your Email and we will send you a password reset link')
     );
   }
