87bc540f14e302fb04a9757c0e2a0aebf6dd7348
joviitt <jovitjohn2004@gmail.com>
2025-03-22 12:01:34 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -53,7 +53,7 @@ class MyApp extends StatelessWidget {
         primaryColor: TColor.primaryColor1,
         fontFamily: "Poppins"
       ),
-      home:LoginView(),
+      home:OnBoardingView(),
     );
   }
 }
